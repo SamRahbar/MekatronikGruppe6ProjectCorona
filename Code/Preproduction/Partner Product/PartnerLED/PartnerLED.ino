8108bce807cfb4acc047247684585427596df8ba
ellenhellden <61236734+ellenhellden@users.noreply.github.com>
2020-06-11 16:12:58 +0200

    Works!!
    We press one of four buttons on the user board and light up an LED-combination on the partner board. Yihaw

--- Code/Preproduction/Partner Product/PartnerLED/PartnerLED.ino
+++ Code/Preproduction/Partner Product/PartnerLED/PartnerLED.ino
@@ -1,4 +1,38 @@
 #include <FastLED.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+// Definerer id og password til netværksforbindelse som NodeMCU anvender
+const char* ssid = "Ellens iPhone"; //Network name
+const char* password = "karamellen"; //Wifipassword
+
+// Definerer information til mqtt serveren
+const char *mqtt_server = "hairdresser.cloudmqtt.com"; //The name of the mqtt site
+const int mqtt_port = 15721; // Defines the mqtt port
+const char *mqtt_user = "Ellen1"; // Defines the mqtt username
+const char *mqtt_pass = "gruppe6go"; //Defines the mqtt code
+
+String payload; // Definerer variablen 'payload' i det globale scope (payload er navnet på besked-variablen)
+
+/////// FUNKTIONSOPSÆTNING ////////
+
+// Opretter en placeholder for callback-funktionen til brug senere. Den rigtige funktion ses længere nede.
+void callback(char* byteArraytopic, byte* byteArrayPayload, unsigned int length);
+
+// Opretter en klient der kan forbinde til en specifik internet IP adresse.
+WiFiClient espClient; // Initialiserer wifi bibloteket ESP8266Wifi, som er inkluderet under "nødvendige bibloteker"
+
+// Opretter forbindelse til mqtt klienten:
+PubSubClient client(mqtt_server, mqtt_port, callback, espClient); // Initialiserer bibloteket for at kunne modtage og sende beskeder til mqtt. Den henter fra definerede mqtt server og port. Den henter fra topic og beskeden payload
+
+/////// FUNKTIONSOPSÆTNING SLUT /////////
+
+int state = 0;
+
+
 
 // How many leds in your strip?
 #define NUM_LEDS 7
@@ -12,13 +46,16 @@
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-void setup() { 
-    // Uncomment/edit one of the following lines for your leds arrangement.
-    // ## Clockless types ##
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+void setup() {
+  // Uncomment/edit one of the following lines for your leds arrangement.
+  // ## Clockless types ##
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  wifiSetup();
+  MQTTSetup();
+  Serial.begin(9600);
 }
 
-void loop() { 
-  fill_solid(leds, NUM_LEDS, CRGB(255,0,255));
-  FastLED.show();
+void loop() {
+  MQTTLoop();
+  LEDloop();
 }
